92ad7f3f4e1d174e59eb46c1cb6fe804ea2ec713
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2015-03-11 13:51:03 +0000

    changing path to URL, for demo-purpose

--- db/src/main/dbschema/default-insert-for-admin_config.sql
+++ db/src/main/dbschema/default-insert-for-admin_config.sql
@@ -3,5 +3,5 @@ INSERT INTO ADMIN_CONFIG VALUES (1,'development', 'is_exif', 'false');
 INSERT INTO ADMIN_CONFIG VALUES (2,'development', 'save_suffix', 'false');
 INSERT INTO ADMIN_CONFIG VALUES (3,'development', 'path_to_files', '/opt/data/mediaserver/demo/');
 INSERT INTO ADMIN_CONFIG VALUES (4,'development', 'base_url', '/MediaServerResteasy/');
+INSERT INTO ADMIN_CONFIG VALUES (6,'development', 'relative_metadata_url', 'media/image');
 INSERT INTO ADMIN_CONFIG VALUES (5,'development', 'relative_stream_url', 'media/stream/');
-INSERT INTO ADMIN_CONFIG VALUES (6,'development', 'relative_metadata_url', '-not-in-use');
