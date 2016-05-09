de75d369941b801f09390eadd9c056b3d9d0f45e
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2016-05-09 12:03:14 +0000

    removing redundant files

--- docs/example-files/aka.sh
+++
@@ -1,3 +0,0 @@
-curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @corvuxcorax.b64 http://127.0.0.1:18080/MediaServerResteasy/media
-curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @picapica.b64 http://127.0.0.1:18080/MediaServerResteasy/media
-curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @picapica-fly.b64 http://127.0.0.1:18080/MediaServerResteasy/media
