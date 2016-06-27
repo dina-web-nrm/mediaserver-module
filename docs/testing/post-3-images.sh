85c99b2decfedcd90b38cf8d9ea1e999f3436025
Ingimar Erlingsson <inkimar@gmail.com>
2016-06-27 12:14:00 +0000

    refactor

--- docs/testing/post-3-images.sh
+++
@@ -1,4 +0,0 @@
-# test to submit three images with metadata to the server
-curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @corvuxcorax.b64 http://localhost:8080/MediaServerResteasy/media | json_pp
-curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @picapica.b64 http://localhost:8080/MediaServerResteasy/media | json_pp
-curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d @picapica-fly.b64 http://localhost:8080/MediaServerResteasy/media | json_pp
