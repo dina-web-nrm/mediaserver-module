caddbce009f538de4cfe96bcd33c61bf212bc998
Ingimar Erlingsson <inkimar@gmail.com>
2019-10-30 22:02:52 +0100

    RESTful GET, fetches a file from the mediaserver

--- clients/src/main/java/se/nrm/bio/mediaserver/clients/ClientGetFile.java
+++ clients/src/main/java/se/nrm/bio/mediaserver/clients/ClientGetFile.java
@@ -10,6 +10,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 /**
+ * INTRO <p> 
  * Make sure that the UUID 'ef1311b1-9bd3-40ea-a794-b124d796024a'
  * <p>
  * is in the database&filesystem
@@ -21,25 +22,42 @@ import org.apache.http.util.EntityUtils;
 public class ClientGetFile {
 
     public static void main(String[] args) throws IOException {
+        ClientGetFile client = new ClientGetFile();
+        CloseableHttpResponse response = client.getFileFromMediaServer("ef1311b1-9bd3-40ea-a794-b124d796024a");
+        int statusCode = response.getStatusLine().getStatusCode();
+
+        System.out.println(response.getProtocolVersion());              // HTTP/1.1
+        System.out.println(response.getStatusLine().getStatusCode());   // 200
+        System.out.println(response.getStatusLine().getReasonPhrase()); // OK
+        System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+
+    }
+
+    /**
+     * using version 2 of the API
+     * <p>
+     * using the format "image/jpeg"
+     * <p>
+     *
+     * @param UUID
+     * @return
+     * @throws IOException
+     */
+    public CloseableHttpResponse getFileFromMediaServer(String UUID) throws IOException {
         String VERSION = "/v2/";
-        String UUID = "ef1311b1-9bd3-40ea-a794-b124d796024a";
         String FORMAT = "image/jpeg";
         String URL = Util.getLocalURL().concat(VERSION).concat(UUID).concat("?format=").concat(FORMAT);
 
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpGet request = new HttpGet(URL);
             try (CloseableHttpResponse response = httpClient.execute(request)) {
-                System.out.println(response.getProtocolVersion());              // HTTP/1.1
-                System.out.println(response.getStatusLine().getStatusCode());   // 200
-                System.out.println(response.getStatusLine().getReasonPhrase()); // OK
-                System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
 
                 HttpEntity entity = response.getEntity();
                 if (entity != null) {
                     String result = EntityUtils.toString(entity);
                     //System.out.println(result);
                 }
-
+                return response;
             }
         }
 
