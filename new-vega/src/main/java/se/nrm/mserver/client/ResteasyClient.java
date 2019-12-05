7eb5ead73099a7b80e3b87eb52e78ab8cabf1905
Ingimar Erlingsson <inkimar@gmail.com>
2019-12-05 18:50:53 +0100

    does not redirect through proxy when proxy has SSL

--- new-vega/src/main/java/se/nrm/mserver/client/ResteasyClient.java
+++ new-vega/src/main/java/se/nrm/mserver/client/ResteasyClient.java
@@ -9,20 +9,28 @@ import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
-import org.apache.http.impl.client.HttpClients;
-
+/**
+ * For testing purposes, hardcoded file in the /tmp-directory. 2019-12-05:
+ * https://www.baeldung.com/httpclient-redirect-on-http-post
+ * - 2019-11-05: redirect i proxy fungerar när jag tar bort ./certs
+ *
+ * @author ingimar
+ */
 public class ResteasyClient {
 
-    // https://stackoverflow.com/questions/18964288/upload-a-file-through-an-http-form-via-multipartentitybuilder-with-a-progress
     public static void main(String[] args) throws IOException {
+        System.out.println("start");
 
         String fileName = "testbild-svt-666.png";
         String filePath = "/tmp/".concat(fileName);
 
-        CloseableHttpClient client = HttpClients.createDefault();
+//        CloseableHttpClient instance = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
+        CloseableHttpClient instance = HttpClientBuilder.create().build();
+
 //        final String url = "http://localhost:8080/MediaServerResteasy/rest/file/vega";
-        final String url = "http://localhost:8080/mserver/rest/file/vega";
+        final String url = "http://media.nrm.se/mserver/rest/file/vega";
         HttpPost post = new HttpPost(url);
         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
         builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
@@ -35,12 +43,16 @@ public class ResteasyClient {
         builder.addTextBody("filename", fileName);
 //        builder.addTextBody("licenseType", "CC-BY");
 
-        final HttpEntity yourEntity = builder.build();
+        final HttpEntity entity = builder.setLaxMode().build();
 
-        post.setEntity(yourEntity);
-        HttpResponse response = client.execute(post);
-        
-        client.close();
-       
+        post.setEntity(entity);
+        HttpResponse response = instance.execute(post);
+
+        int statusCode = response.getStatusLine().getStatusCode();
+
+        System.out.println("\n response ".concat(Integer.toString(statusCode)));
+        System.out.println("response ");
+
+        instance.close(); // Httpclient fundamentals
     }
 }
